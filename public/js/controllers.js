167251a0ea6a90bb4a488cab5c4f77f11ed563eb
Michal Luscon <mluscon@redhat.com>
2014-10-08 21:45:26 +0200

    Implement filter in messages view

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,6 +6,15 @@ RminerApp.controller('MessagesCtrl', function ($scope, $http) {
   $http.get("/messages/?json")
   .success(function(response) {$scope.messages = angular.fromJson(response);});
   
+  $scope.regExpString = ""
+  $scope.sensitivity = 1
+    
+  
+  $scope.myFilter = function(msg) {
+    $scope.regExp = new RegExp($scope.regExpString)
+    var res = $scope.regExp.test(msg.body)
+    return res
+  }
   
   
 }); 
