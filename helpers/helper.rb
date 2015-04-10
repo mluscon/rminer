9fa86fc051798d518c201b44673cae059f7b64d7
Michal Luscon <mluscon@redhat.com>
2015-04-10 21:55:47 +0200

    Fix helper

--- helpers/helper.rb
+++ helpers/helper.rb
@@ -2,9 +2,10 @@ require 'json'
 
 def body_split(body)
   words = []
+  var_reg = /(?<=>>).*(?=>>>)/
   body.split(" ").each_with_index do |word, i|
     if word.match(/^<<</)
-      words.push({"word" => word, "variable" => true})
+      words.push({"word" => var_reg.match(word), "variable" => true})
     else
       words.push({"word" => word, "variable" => false})
     end
