ed020dc6a71b8d9e4c3a2b17f6638332b934ff47
Michal Luscon <mluscon@redhat.com>
2015-05-19 23:04:25 +0200

    Introduce better config

---
+++ config.rb
@@ -0,0 +1,32 @@
+require 'parseconfig'
+
+class RminerConf
+  attr_reader(
+    :amqp_server,
+    :amqp_channel,
+    :workers,
+    :cache_size,
+    :logfile,
+    :host,
+    :database,
+    :user,
+    :password,
+    :adapter
+  )
+
+
+  def initialize
+    config = ParseConfig.new('./rminer.conf')
+    @amqp_server = config['amqp_server'] or 'localhost'
+    @amqp_channel = config['amqp_channel'] or 'rminer'
+    @workers = config['workers'].to_i or 1
+    @cache_size = config['cache'].to_i or 1000
+    @logfile = config['logfile'] or './logs/rminer.log'
+
+    @host = config['db_host'] or 'localhost'
+    @database = config['db_database'] or 'rminer'
+    @user = config['db_user'] or 'rminer'
+    @password = config['db_password'] or 'rminer'
+    @adapter = config['db_adapter'] or 'postgres'
+  end
+end
